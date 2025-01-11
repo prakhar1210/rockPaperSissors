50d2b283d4ff32a2c4fe8703b831c5a25281879b
Prakhar <prakharshrivastava806@gmail.com>
2025-01-11 18:21:27 +0530

    numbers and function

---
+++ demo.py
@@ -0,0 +1,40 @@
+# # a=2
+# # b=3
+# # c=a*b
+# # print(c)
+
+
+# print("hello Prakhar!!!")
+
+
+
+#print(5*8/2+6-2)
+
+# PRAKHAR =25
+# print(PRAKHAR * 5)
+
+# True
+#Falsess
+
+# name = input("what is your name ?")
+# print("Hello " + name+ "!")
+
+# age= int(input("enter your age"))
+# print(age)
+
+# def request_age():
+#     age=input("enter your age")
+#     return int(age)
+
+# user_age = request_age()
+
+# demo.py
+
+def request_age():
+    age = input("Enter your age: ")
+    return int(age)
+
+# Call the function
+user_age = request_age()
+print(f"You are {user_age} years old!")
+
