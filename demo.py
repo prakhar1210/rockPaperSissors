849937bb565051cc26cffdd5b382ace33fc89a25
Prakhar <prakharshrivastava806@gmail.com>
2025-01-12 13:33:26 +0530

    functions and some more things

--- demo.py
+++ demo.py
@@ -30,11 +30,49 @@
 
 # demo.py
 
-def request_age():
-    age = input("Enter your age: ")
-    return int(age)
+# def request_age():
+#     age = ''
+#     while age.isdigit() == False :
+#      age = input("Enter your age: ")
+#      if age.isdigit() == False:
+#         print("Enter a valid age")
+#     return int(age)
 
-# Call the function
-user_age = request_age()
-print(f"You are {user_age} years old!")
+# # Call the function
+# user_age = request_age()
+# print(f"You are {user_age} years old!")
 
+def play_again():
+    """this is a programe which takes y or n as an input"""
+    answer = ''
+
+    while answer.lower() not in ["y", 'n']:
+        answer = input("do you want to play again? : ")
+
+        if answer.lower() == 'y':
+            return True
+        elif answer.lower() == 'n':
+            return False
+        else:
+            print("Enter Y or N only. please try again")
+
+# call the function
+# play_again()
+# print(play_again__doc__)
+# type(play_again)
+
+
+def welcome():
+    message = 'welcome to the world of python!!'
+    hello = message.upper()
+    print(hello)
+
+# welcome()
+
+
+
+def play_again(answer):
+    if answer == 'yes':
+        return True
+    else:
+        return False
